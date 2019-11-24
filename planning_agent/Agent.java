2139e00fb138810bd3ad9af2aff0faf30c99d9bf
Bennygmate <Benjamin.Cheung8@Outlook.com>
2019-11-24 18:51:18 +1100

    Update Agent.java

--- planning_agent/Agent.java
+++ planning_agent/Agent.java
@@ -1,19 +1,10 @@
 
 /***********************************************
  *  Agent.java 								   *
- *  Sample Agent for Text-Based Adventure Game *
- *  COMP3411 Artificial Intelligence		   *
- *  UNSW Session 1, 2017					   *
- *  z3460693                                   *
+ *  Sample Agent for Text-Based Adventure Game *                                *
 ************************************************/
 
-/**********************************************************************************
- *  Honestly, I am quite new to Java and object orientated programming but wanted *
- *  to practise in this assignment so might be a bit messy. The program has got a *
- *  few bugs, if rotate agent in different directions, not too sure how to fix    *
- *  sometimes it passes the tests (going have to rely on luck for some) 		  *
- *  The program is also a bit slow, not too sure how to look at time complexities *
- *  in java (seems to be bit slow compared to C)			            		  *
+/**********************************************************************************	            		  *
  *  Algorithms Used: was AStar Search with Manhanttan Distance Heuristic 		  *
  *  (manipulated to suit different costs of agent environment)					  *
  *  and seed fill to check if specific locations are reachable or not			  *
@@ -27,26 +18,11 @@
  *  map,some logical inferences on agent moves and whether or not is it possible  *
  *  Agent Planner Class - Handles the goals/utilites through different priorities *
  *  AStar Class - Helps agent plan paths (State-Based Search) through different   *
- *  goals/utilties as well.										     			  * 
+ *  goals/utilties as well.							 * 
  *  Agent Reach Class - Helps the agent plan (State-Based Search) whether it is   *
  *  logical to reach a certain state (makes program bit slow because of this)     *
  *  Agent Explorer Class - Tries to expand the agent's perception/knowledge base  *
- *  through exploring the hidden environment								      *
- *  In the end, it is the agent planner who feeds to the action of the agent      *
- *  (would be helpful if world model could feed as well)			   		      *
- *  (world model, agent planner, agent action, agent perception) to store 		  *
- *  different knowledge bases within that the agent knows will be a priority      *
- *  Along the way, I tried to make different data structures for storing          *
- *  (redesigning whole program) but kept getting confused, as it is a             *
- *  bit slow and buggy I tried to play around with different g costs for          *
- *  different environmental obstacles within the AStar algorithm but it           *
- *  seemed to be quite confusing as the agent has to handle a lot of different    *
- *  inferences such as conservation of trees for a raft, blasting dynamites       *
- *  at the right objects, gaining dynamites, spatial orientations, would be great *
- *  if I used a strategy pattern, so it wouldn't be as confusing with many        *
- *  different functions but me myself was trying to reduce the time as sometimes  *
- *  the program runs out of heap space and then also couldn't pass some tests     *
- *  so I redesigned the assignment for 2 weeks and kept getting frustrated.       *
+ *  through exploring the hidden environment					 *
 ***********************************************************************************/
 
 import java.util.*;
